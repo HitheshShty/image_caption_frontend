703a8cf7c95d125bac660296128b4709b8f698e6
HITHESH SHETTY <shettyhithesh22@gmail.com>
2024-07-03 22:02:35 +0530

    Added all files

---
+++ test_back.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello/")
+def welcome_message():
+    return {"message": "The server is up"}
